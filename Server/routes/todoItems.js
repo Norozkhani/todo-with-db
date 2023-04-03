b7867dc5d692f2eb91ca5d28dd54f590e85ba698
Helena <helena@sixfeetcreative.com>
2023-04-03 09:57:07 +0200

    todoItems added

---
+++ Server/routes/todoItems.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+async function main() {
+  const sequelize = new Sequelize(
+    "postgres://hyper:Rox6kTdcxcRzPeEWB6Ff@104.199.12.40:5432/malena"
+  );
+
+  const TodoItemSchema = new sequelize.createSchema
+
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+main();
