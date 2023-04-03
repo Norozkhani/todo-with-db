c33b5fff5c84c91cc3dad85e28ebbc909d75bfef
Helena <helena@sixfeetcreative.com>
2023-04-03 10:16:43 +0200

    added tasks.js

---
+++ Client/src/components/Tasks.js
@@ -0,0 +1,36 @@
+import Task from "./Task";
+import "../styles/tasks.css";
+
+const Tasks = ({ tasks, handleCheck, activateEditTask }) => {
+  return (
+    <div>
+      <h2>Incomplete</h2>
+      <ul>
+        {tasks
+          .filter((task) => task.completed === false)
+          .map((task) => (
+            <Task
+              task={task}
+              key={task.id}
+              handleCheck={handleCheck}
+              activateEditTask={activateEditTask}
+            />
+          ))}
+      </ul>
+      <h2>Complete</h2>
+      <ul>
+        {tasks
+          .filter((task) => task.completed === true)
+          .map((task) => (
+            <Task
+              task={task}
+              key={task.id}
+              handleCheck={handleCheck}
+              activateEditTask={activateEditTask}
+            />
+          ))}
+      </ul>
+    </div>
+  );
+};
+export default Tasks;
