69866698509aff3584746883b0f04e9470e90417
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-04-19 12:52:20 +0200

    get cors

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,5 +1,6 @@
 import Task from "./task";
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
